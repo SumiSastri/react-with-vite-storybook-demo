f60890738974590c5591139cbd4e117fab8782fb
Sumi-CRUK-dev <sumi.sastri@cancer.org.uk>
2023-11-14 18:44:33 +0000

    Initial commit - update docs and set up component

---
+++ storybook-demo/src/components/onlyStories.tsx
@@ -0,0 +1,11 @@
+interface Props {}
+
+function OnlyStories(props: Props) {
+    const {} = props
+
+    return (
+        <div>This component uses Storybook stories exclusively with Chakra CSS</div>
+    )
+}
+
+export default OnlyStories
