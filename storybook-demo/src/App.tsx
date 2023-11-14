9ebd37353fb1bbdcfb6e57206ccf52e9aa59feb5
Sumi-CRUK-dev <sumi.sastri@cancer.org.uk>
2023-11-14 11:43:50 +0000

    Add style props to chakra components and a sateful button

--- storybook-demo/src/App.tsx
+++ storybook-demo/src/App.tsx
@@ -2,8 +2,8 @@ import { useState } from 'react'
 
 // styles
 import './App.css'
-import './scss/main.scss'
-import { Card, Button, Divider } from '@chakra-ui/react'
+// import './scss/main.scss'
+import { Card, Button, Divider, Box, Text } from '@chakra-ui/react'
 
 // components
 import HelloWorld from './components/helloWorld'
@@ -16,14 +16,25 @@ function App() {
       <div>
         <Card>
              <HelloWorld/>
-   <Button>Chakra Button</Button>
+   <Button onClick={() => setCount((count) => count + 1)}>Chakra Stateful Button ... click to change counter number {count}</Button>
    <Divider/>
-   <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+
+<p>adding linear gradient and color transitions</p> 
+<Box w="100%" h="200px" bgGradient="linear(to-t, green.200, pink.500)" />
+
+<p>adding radial gradient and color transitions</p>
+<Box w="100%" h="200px" bgGradient="radial(gray.300, yellow.400, pink.200)" />
+
+<p>adding the text gradient</p>
+<Text
+  bgGradient="linear(to-l, #7928CA, #FF0080)"
+  bgClip="text"
+  fontSize="6xl"
+  fontWeight="extrabold"
+>
+  Straight out of Chakra UI Style Props docs
+</Text>
+    
         </Card>
       </div>
   )
